ea59a9983a4a7d20b2dad0965820d3d1b1c427f6
Walter Silva <edweb.mza@gmail.com>
2024-10-18 10:49:45 -0300

    ejemplo de condicionales

--- js/index.js
+++ js/index.js
@@ -1,28 +1,39 @@
                                     //variables
 
-let nombre = 'walter';
-let apellido = 'silva';
-let edad = '37';
+// let nombre = 'walter';
+// let apellido = 'silva';
+// let edad = '37';
 
-console.log (nombre);
-console.log (apellido);
-console.log (edad);
+// console.log (nombre);
+// console.log (apellido);
+// console.log (edad);
 
 
                                         //ecuaciones
                                         
-let numeroA = 2;
-let numeroB = 3;
-let numeroC = 4;
-const numero3 = 5;
+// let numeroA = 2;
+// let numeroB = 3;
+// let numeroC = 4;
+// const numero3 = 5;
 
-let resutladoSuma = numeroA + numeroB;
-let resultadoResta = numeroA - numeroB;
-let resultadoProducto = numeroA * numeroC;
+// let resutladoSuma = numeroA + numeroB;
+// let resultadoResta = numeroA - numeroB;
+// let resultadoProducto = numeroA * numeroC;
 
-console.log (resutladoSuma);
-console.log (resultadoResta);
-console.log (resultadoProducto);
+// console.log (resutladoSuma);
+// console.log (resultadoResta);
+// console.log (resultadoProducto);
 
 
+                                        //condicionales
 
+let unNumero = 7;
+
+if (unNumero == 5) {
+    console.log ("Hola Bro");
+} else if (unNumero == 6){
+    console.log ("Bye Bro");
+
+} else {
+    console.log ('Bye Bro');
+}
