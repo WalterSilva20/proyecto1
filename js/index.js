0dc2402a7544ab6dc3d7cf71a893447cb260a22b
Walter Silva <edweb.mza@gmail.com>
2024-10-18 10:21:48 -0300

    aprendiendo ecuaciones

--- js/index.js
+++ js/index.js
@@ -1,7 +1,28 @@
-let nombre = 'wally';
+                                    //variables
+
+let nombre = 'walter';
 let apellido = 'silva';
 let edad = '37';
 
 console.log (nombre);
 console.log (apellido);
 console.log (edad);
+
+
+                                        //ecuaciones
+                                        
+let numeroA = 2;
+let numeroB = 3;
+let numeroC = 4;
+const numero3 = 5;
+
+let resutladoSuma = numeroA + numeroB;
+let resultadoResta = numeroA - numeroB;
+let resultadoProducto = numeroA * numeroC;
+
+console.log (resutladoSuma);
+console.log (resultadoResta);
+console.log (resultadoProducto);
+
+
+
