da46d06f10714c355b1705981c853b86f1bd9f2d
Walter Silva <edweb.mza@gmail.com>
2024-10-19 13:07:29 -0300

    Estructura For, Ejemplo de Práctica y aplicando Tablas, según un Número Ingresado

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-                                    //variables
+                                        //variables
 
 // let nombre = 'walter';
 // let apellido = 'silva';
@@ -17,7 +17,7 @@
 // const numero3 = 5;
 
 // let resutladoSuma = numeroA + numeroB;
-// let resultadoResta = numeroA - numeroB;
+// let resultadoResta = numeroA - numeroB; 
 // let resultadoProducto = numeroA * numeroC;
 
 // console.log (resutladoSuma);
@@ -27,13 +27,55 @@
 
                                         //condicionales
 
-let unNumero = 7;
+// let unNumero = 7;
 
-if (unNumero == 5) {
-    console.log ("Hola Bro");
-} else if (unNumero == 6){
-    console.log ("Bye Bro");
+// if (unNumero == 5){
+//     console.log ("Hola Bro");
+// }else if (unNumero == 6){
+//     console.log ("Bye Bro");
 
-} else {
-    console.log ('Bye Bro');
+// }else {
+//     console.log ('Bye Bro');
+// }
+
+// let nombreUsuario = prompt("ingresar nombre de usuario");
+
+// if (nombreUsuario == ""){
+//     alert("No ingresaste nombre de usuario");
+// }else {
+//     alert("Nombre de usuario:" + " " + nombreUsuario);
+// }
+
+// let precio = parseInt(prompt("Ingresa un Precio"));
+
+// if (precio < 20){
+//     alert("Precio menor que 20");
+// }else if (precio <40){
+//     alert("Precio menor que 40");
+// }else if (precio <60){
+//     alert("Precio menor que 60");
+// }else if (precio >100){
+//     alert("Precio mayor que 100");
+// }
+
+
+                            //Tipos de Bucles: Estructura For 
+
+// for (let i = 0; i< 10; i++) {
+//     alert (i);
+// }
+
+                            // ahora cuenta de 1 a 10
+
+// for (let i = 1; i<=10; i++){
+//     alert (i)
+// }
+
+                            // Algoritmo para calcular la tabla del multiplicar de un número
+
+
+let ingresarNumero = parseInt(prompt("Ingresar Número"));
+for (let i = 1; i <=10; i++) {
+    let resultado = ingresarNumero * i ;
+    alert(ingresarNumero + "x"+ i +" = "+ resultado);
 }
